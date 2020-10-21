1a4ff2ccc6c0cce6d1ddb3068656299bde5ca8b7
Mihai Chiru <mihai@greenlytics.io>
2020-10-21 15:45:36 +0000

    Small update to the wind run script

--- run_gefcom2014_wind.sh
+++ run_gefcom2014_wind.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Select params.json file
-PARMS=${1:-'./params/params_gefcom2014_wind_competition_v1.json'}
+PARMS=${1:-'./params/params_gefcom2014_wind_example.json'}
 
 # Extract gefcom2014 data
-# python -W ignore ./preprocess/extract_gefcom2014_wind_solar_load.py wind
+python -W ignore ./preprocess/extract_gefcom2014_wind_solar_load.py wind
 
 # Preprocess gefcom2014 data
-# python -W ignore ./preprocess/preprocess_gefcom2014_wind_example.py $PARMS
+python -W ignore ./preprocess/preprocess_gefcom2014_wind_example.py $PARMS
 
 # Train model
 python -W ignore ./gbdt_forecast.py $PARMS
